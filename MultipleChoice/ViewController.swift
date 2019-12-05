6454efbd60415e609b19f1898921c80ab5a04bc3
Sebastian Tyson <styson@lcs.on.ca>
2019-12-05 14:43:40 -0500

    Added outlets, interface and guard statement

--- MultipleChoice/ViewController.swift
+++ MultipleChoice/ViewController.swift
@@ -10,11 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var amountOfQuestions: UITextField!
+    @IBOutlet weak var studentAnswer: UITextField!
+    @IBOutlet weak var correctAnswers: UITextField!
+    @IBOutlet weak var studentsResults: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+  
+    @IBAction func answerChecker(_ sender: Any) {
+        
+        // guard statements making sure there is an input in the box, that its more that zero and lets the questions become an integer
+        guard let questionCount = amountOfQuestions.text,
+                questionCount.count > 0,
+                let questions = Int(questionCount)  else {
+            studentsResults.text = "PLease enter a valid amount of questions"
+            return
+        }
+        
+        
+        
+    }
+    
+    
 }
+
+
 
